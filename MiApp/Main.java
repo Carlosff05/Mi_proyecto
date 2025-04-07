accab4e27e745fa68355557fe4ced3b29d556759
Carlosff05 <carlosff05@gmail.com>
2025-04-07 10:45:10 +0200

    El main esta funcionando

--- MiApp/Main.java
+++ MiApp/Main.java
@@ -1,5 +1,8 @@
 public class Main {
-    public static void main(Args[] String) {
-        System.out.println("Esto es un print")
+    public static void main(String[] args) {
+        System.out.println("Esto es un print");
+        Coche c1 = new Coche("Loque sea", 20);
+
+        c1.acelerar(20);
     }
 }
