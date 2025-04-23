d0194e82f329cec92614d64d47edc229f5652f7f
Carlosff05 <carlosff05@gmail.com>
2025-04-23 08:54:04 +0200

    Revert "El main esta funcionando"
    This reverts commit accab4e27e745fa68355557fe4ced3b29d556759.

--- MiApp/Main.java
+++ MiApp/Main.java
@@ -1,8 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Esto es un print");
-        Coche c1 = new Coche("Loque sea", 20);
-
-        c1.acelerar(20);
+    public static void main(Args[] String) {
+        System.out.println("Esto es un print")
     }
 }
