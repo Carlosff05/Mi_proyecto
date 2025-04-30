d49abc14a3f926166a2416c7ee5402eca5dc4604
Carlosff05 <carlosff05@gmail.com>
2025-04-30 08:55:29 +0200

    nuevo commit

--- MiApp/Main.java
+++ MiApp/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] Args) {
         System.out.println("Esto es un print");
+
+        Coche rojo = new Coche("aljdfaljfdaljf", 120);
     }
 }
